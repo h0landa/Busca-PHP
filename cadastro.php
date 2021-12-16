7d70a05aa0419b60af3c155ed9047a58d4b92d58
Arthur Holanda <holandaarthur46@gmail.com>
2021-12-16 17:30:46 -0300

    Adicionando o Style.css

--- cadastro.php
+++ cadastro.php
@@ -5,34 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
     <title>Cadastro de Zonas</title>
+
 </head>
-<style>
-    h1{
-        color:blue;
-        align: center;
-        width: 1000px;px;
-        margin-top: 100px;
-        margin-right: 10000px;
-        margin-left: 400px;
-        
-    }
-    body{
-        background-color:yellow;
-        color:black;
-    
-    }
+
 </style>
-<h1>Cadastro de Zonas</h1>
-<body>
+<h1 class="h1">Cadastro de Zonas</h1>
+<body class="body">
 <form>
-  <div class="form-group" style="margin: 100px 800px 40px 150px;">
-    <label for="area_name">Nome da Área</label>
-    <input type="text" class="form-control" id="area_name" placeholder="Insira a área" style="width: 800px;
-    margin-left: 100px">
+  <div class="form-group">
+    <label for="area_name" class="cadastro">Nome da Área</label>
+    <input type="text" class="form-group" id="area_name" placeholder="Insira a área">
+    <button type="submit" class="btn btn-primary">Cadastrar</button>
   </div>
-  <br>
-  <button type="submit" class="btn btn-primary" style="margin: 150px;">Cadastrar</button>
 </form>    
 </body>
 </html>
