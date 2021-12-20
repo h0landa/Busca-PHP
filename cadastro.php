0bc7d9d7a4b3f6a63c7e292b661f83e37a16ea76
Arthur Holanda <holandaarthur46@gmail.com>
2021-12-20 17:27:35 -0300

    Melhorias

--- cadastro.php
+++ cadastro.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="style.css" rel="stylesheet">
-    <title>Cadastro de Zonas</title>
+    <link href="estilo/style.css" rel="stylesheet">
+    <title class="titulo">Cadastro de Zonas</title>
 </head>
 
 </style>
@@ -19,6 +19,7 @@
   <select type="select" id="input_zonas" name="zonas_select">
     <option value="CM01">CM01</option>
     <option value="CM02">CM02</option>
+    <option value="CM03">CM03</option>
     <option value="SJ01">SJ01</option>
     <option value="SJ02">SJ02</option>
     <option value="SJ03">SJ03</option>
@@ -30,7 +31,20 @@
     <option value="JC03">JC03</option>
     <option value="JC04">JC04</option>
     <option value="JC05">JC05</option>
+    <option value="JC06">JC06</option>
+    <option value="JC01">SJ01</option>
+    <option value="JC02">SJ02</option>
+    <option value="JC03">SJ03</option>
+    <option value="JC04">SJ04</option>
+    <option value="JC05">SJ05</option>
     <option value="JC06">SJ06</option>
+    <option value="EX01">EX01</option>
+    <option value="TR01">TR01</option>
+    <option value="TR02">TR02</option>
+    <option value="JC06">PU01</option>
+    <option value="MC01">MC01</option>
+    <option value="NT01">NT01</option>
+    <option value="AB01">AB01</option>
   </select>
     <!--<label for="area_name" class="cadastro">Nome da Área</label>-->
     <input type="text" name="n_area" placeholder="Insira a área" class="inserir-area">
--- index.php
+++ index.php
@@ -10,28 +10,12 @@ $con = $mysqli->query($consulta) or die($mysqli->error);
 <head>
 	<meta charset="UTF-8">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link href="estilo/style_index.css" rel="stylesheet">
 	<title>Buscar Zonas</title>
 </head>
-<style>
-	body{
-		background-color:black;
-		color:orange;
-	}
-	table,th,td{
-		background-color:black;
-		border: 2px solid orange;
-		color:White;
-			
-	}
-	tr,th{
-		background-color:orange);
-		
-		
-	}
-</style>
-<body>
-	<h2>Buscador de Zonas</h2>
-	<form name="PesquisaZonas" onsubmit="return validarFormulario()"action="busca.php" method="GET">
+
+<body class="corpo">
+	<form name="PesquisaZonas" onsubmit="return validarFormulario()"action="busca.php" method="GET" class="formulario_pesquisa">
 		<div class="input-group mb-3" style="width:800px;">
   			<span class="input-group-text" id="inputGroup-sizing-default">Nome da Área</span>
   			<input type="text" name="nome_area" class="form-control" placeholder="Insira o nome da Área" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
@@ -59,11 +43,13 @@ $con = $mysqli->query($consulta) or die($mysqli->error);
 </script>
 
 	<table class="table">
+		<thead class="thead-dark">
 		<tr>
 			<th>id</th>
 			<th>Zonas</th>
 			<th>Áreas</th>
 		</tr>
+</thead>
 		<?php while ($dado = $con->fetch_array()){ ?>
 		<tr>
 			<td><?php echo $dado["id"];?></td>
