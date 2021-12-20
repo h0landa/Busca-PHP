f29775029ed288e203eb92bfd921e47d2453b4c0
Arthur Holanda <holandaarthur46@gmail.com>
2021-12-20 13:38:56 -0300

    melhoras na estilização
    Melhoras no estilo da tela de cadastro.

--- cadastro.php
+++ cadastro.php
@@ -10,28 +10,30 @@
 </head>
 
 </style>
-<h1 class="h1">Cadastro de Zonas</h1>
 <body class="body">
 <form name="CadastroZonas" method="GET" action="seleciona_zona.php">
-  <div class="form-group">
-    <select type="select" class="select_zonas" id="input_zonas" name="zonas_select">
-      <option value="CM01">CM01</option>
-      <option value="CM02">CM02</option>
-      <option value="SJ01">SJ01</option>
-      <option value="SJ02">SJ02</option>
-      <option value="SJ03">SJ03</option>
-      <option value="SJ04">SJ04</option>
-      <option value="SJ05">SJ05</option>
-      <option value="SJ06">SJ06</option>
-      <option value="JC01">JC01</option>
-      <option value="JC02">JC02</option>
-      <option value="JC03">JC03</option>
-      <option value="JC04">JC04</option>
-      <option value="JC05">JC05</option>
-      <option value="JC06">SJ06</option>
-    </select>
-    <label for="area_name" class="cadastro">Nome da Área</label>
-    <input type="text" class="form-group" name="n_area" placeholder="Insira a área">
+  <div class="container">
+  <h1 class="h1">Cadastro de Zonas</h1>
+  <!--pode tirar esse br se vc quiser-->
+  <br>
+  <select type="select" id="input_zonas" name="zonas_select">
+    <option value="CM01">CM01</option>
+    <option value="CM02">CM02</option>
+    <option value="SJ01">SJ01</option>
+    <option value="SJ02">SJ02</option>
+    <option value="SJ03">SJ03</option>
+    <option value="SJ04">SJ04</option>
+    <option value="SJ05">SJ05</option>
+    <option value="SJ06">SJ06</option>
+    <option value="JC01">JC01</option>
+    <option value="JC02">JC02</option>
+    <option value="JC03">JC03</option>
+    <option value="JC04">JC04</option>
+    <option value="JC05">JC05</option>
+    <option value="JC06">SJ06</option>
+  </select>
+    <!--<label for="area_name" class="cadastro">Nome da Área</label>-->
+    <input type="text" name="n_area" placeholder="Insira a área" class="inserir-area">
     <button type="submit" class="btn btn-primary">Cadastrar</button>
   </div>
 </form>
